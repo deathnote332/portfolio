cb286875f2473265cdf3db92748aeea24533cf1b
John Paul Inhog <john.inhog@republisys.com>
2017-07-25 15:10:03 +0800

    update

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,7 +8,7 @@ use Theme;
 class ProfileController extends Controller
 {
     public function profile(){
-        $theme = Theme::uses('default')->layout('default')->setTitle('PapersLLC');
+        $theme = Theme::uses('default')->layout('default')->setTitle('JPBI');
         return $theme->scope('profile')->render();
     }
 }
